60c7787ae87ddf5beca528ee16fe805a98390438
takunoumi <takunoumi.r@gmail.com>
2020-06-16 17:09:35 +0900

    値の受け渡し処理についての記述を追加

--- ScheduleManagement/src/main/java/jp/co/jrqss/admin/schedule/controller/ScheduleController.java
+++ ScheduleManagement/src/main/java/jp/co/jrqss/admin/schedule/controller/ScheduleController.java
@@ -1,13 +1,42 @@
 package jp.co.jrqss.admin.schedule.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jp.co.jrqss.admin.schedule.domain.model.Building;
+import jp.co.jrqss.admin.schedule.domain.model.Employee;
+import jp.co.jrqss.admin.schedule.domain.model.Work;
+import jp.co.jrqss.admin.schedule.domain.service.BuildingService;
+import jp.co.jrqss.admin.schedule.domain.service.EmployeeService;
+import jp.co.jrqss.admin.schedule.domain.service.WorkService;
+
 @Controller
 public class ScheduleController {
+
+	@Autowired
+	WorkService workService;
+
+	@Autowired
+	BuildingService buildingService;
+
+	@Autowired
+	EmployeeService employeeService;
+
 	@GetMapping("/admin/schedule")
-	public String getSchedule() {
+	public String getSchedule(Model model) {
+		List<Work> workList = workService.getWorkInfo();
+		List<Building> buildingList = buildingService.getBuildingInfo();
+		List<Employee> employeeList = employeeService.getEmployeeInfo();
+
+		model.addAttribute("workList",workList);
+		model.addAttribute("buildingList",buildingList);
+		model.addAttribute("employeeList",employeeList);
+
 		return "admin/schedule/list";
 	}
 
