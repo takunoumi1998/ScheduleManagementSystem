201521529f182af3d5aa926c4e4be13e98561465
Yuri Miura <yuri.lily.1207@gmail.com>
2020-06-17 10:18:42 +0900

    ログ出力とエラー文の文字コード
    念のためつくっておきました

---
+++ ScheduleManagement/src/main/java/jp/co/jrqss/LogAspect.java
@@ -0,0 +1,31 @@
+package jp.co.jrqss;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+
+	@Around("@within(org.springframework.stereotype.Controller)")
+    public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+
+        System.out.println("メソッド開始： " + jp.getSignature());
+
+        try {
+            // ポイント２：メソッド実行
+            Object result = jp.proceed();
+
+            System.out.println("メソッド終了： " + jp.getSignature());
+
+            return result;
+
+        } catch (Exception e) {
+            System.out.println("メソッド異常終了： " + jp.getSignature());
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
